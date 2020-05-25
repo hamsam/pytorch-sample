d608d2cedbd79ab23a0a6e62f7d8dcde498e96f7
CBNUAILAB <39152481+CBNUAILAB@users.noreply.github.com>
2020-05-25 18:45:06 +0900

    Update Split_dataset.py

--- Split_dataset.py
+++ Split_dataset.py
@@ -63,6 +63,7 @@ file1.close
     
 # All_file_metadata.close()
 
+#기존 파일 dic, 레이블 dic 저장
 # metadata shuffle
 # path = './all_file_metadata.txt'
 # meta_list = []
@@ -74,9 +75,84 @@ file1.close
 
 # import random
 
+#메타 데이터 파일 셔플
 # random.shuffle(meta_list)
 # All_file_metadata_shuffle = open('./all_file_metadata_shuffle.txt', 'w')
 # for item in meta_list :
 #     data = str(item[0]) + " " + str(item[1]) +"\n"
 #     All_file_metadata_shuffle.write(data)
 # All_file_metadata_shuffle.close
+
+
+# #셔플된 메타 데이터 파일 기반으로, 데이터셋/레이블 파일 분배 및 저장,
+import shutil
+
+path = './all_file_metadata_shuffle.txt'
+
+shuffle_meta = []
+saved_file_dic = []
+with open(path, 'r') as f:
+    for line in f.readlines()[0:] :
+        v = line.strip().split()
+        shuffle_meta.append(v)
+        
+print(shuffle_meta[0])
+
+train_label = open('./data/train/train_label.txt', 'w', encoding='utf8')
+validate_label = open('./data/validate/validate_label.txt', 'w', encoding='utf8')
+test_label = open('./data/test/test_label.txt', 'w', encoding='utf8')
+
+ratio_dataset = [11340,2430,2430]
+print(sum(ratio_dataset))
+
+
+
+count = 1
+save_file = train_label
+phase = 'train/'
+
+num =0
+num2 =0
+num_e =[0,0,0]
+for item in shuffle_meta :
+    split_file_dic = item[0].split('\\')
+    only_file_name = split_file_dic[3]
+    
+    lf = open(item[1], 'r', encoding='UTF8')
+    
+    label = lf.readline()
+    
+    sentence = label.strip().split()
+    #print(len(word))
+    # if len(word) == 1:
+    #     num +=1
+    # else :
+    #     num2+=1
+    #     if phase == 'train/' :
+    #         num_e[0] +=1
+    #     elif phase == 'validate/' :
+    #         num_e[1] +=1
+    #     elif phase =='test/' :
+    #         num_e[2] +=1
+    label = ''
+    for word in sentence :
+        label += word
+    #print(label)
+
+    # 각 데이터셋 레이블 파일에 파일 저장될 경로, 실제 레이블 저장
+    data = str('./' +only_file_name) + " " + label +"\n"
+
+    save_file.write(data)
+    #shutil.copy(item[0], './data/'+phase+only_file_name)
+    if count == ratio_dataset[0] :
+        save_file = validate_label
+        phase = 'validate/'
+        
+    if count == ratio_dataset[0] +ratio_dataset[1] :
+        save_file = test_label
+        phase = 'test/'
+        
+    count+=1
+
+print(num, num2)
+print(num_e)
